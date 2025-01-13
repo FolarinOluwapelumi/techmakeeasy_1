8e3e0bd5253e56917291527ab1a495d519083d86
Folarin <folarinoluwapelumi81@gmail.com>
2025-01-13 12:52:25 +0000

    hero changes-2

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -4,7 +4,7 @@ import hero from "../assets/hero.png"
 
 const HeroSection = () => {
   return (
-    <main className="flex flex-col-reverse lg:flex-row justify-between items-center bg-gradient-to-r from-blue-400 to-purple-700 min-h-[80vh] px-6 md:px-12 py-16 gap-8">
+    <main className="flex flex-col-reverse lg:flex-row justify-between items-center bg-gradient-to-r from-blue-400 to-purple-700 h-screen sm:h-[80vh] px-6 md:px-12 py-16 gap-8">
       {/* Content Section */}
       <div className="flex-1 text-white">
         <h2 className="sm:text-5xl text-4xl font-bold mb-6 text-center sm:text-start">
@@ -31,7 +31,7 @@ const HeroSection = () => {
 
       {/* Illustration Section */}
       <div className="flex-1 flex justify-center items-center">
-       <img src={hero} />
+       <img src={hero} className="sm:h-[80vh]"/>
       </div>
     </main>
   );
