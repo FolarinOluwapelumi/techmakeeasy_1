c859a024938a608f3fafff364d4f59301332ae71
Folarin <folarinoluwapelumi81@gmail.com>
2025-01-24 09:03:00 +0000

    removed unnnecessary code

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -15,16 +15,6 @@ const HomePage = () => {
     <div className="min-h-screen text-white ">
       <Header />
       <HeroSection />
-      {/* Hero Section
-      <main className="flex flex-col justify-center items-center bg-gradient-to-r from-blue-400 to-purple-700 h-[80vh] px-4 md:px-8">
-        <h2 className="sm:text-5xl text-4xl font-bold text-center mb-6">
-         Success Made Easy With <br /> TechMakeEasy.
-        </h2>
-        <p className="sm:text-center text-justify leading-relaxed font-medium text-lg text-gray-200 sm:text-white max-w-lg">
-          Transform your business with a well-designed website and innovative ideas that drive growth and engagement.
-        </p>
-      </main> */}
-
       <ServicesSection />
       <ProjectsSection />
       <IndustrySection />
